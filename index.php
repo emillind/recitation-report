7fb083a95ac749cab0b62a6574310d3130164d07
Jonathan Berglind <jonathan.berglind@gmail.com>
2016-02-24 21:34:30 +0100

    Bootstrap framework added

--- index.php
+++ index.php
@@ -2,8 +2,15 @@
 <html>
 <head>
 	<title>Recitation Reporter</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
+	<div class="container">
 
+	</div>
 </body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
 </html>
